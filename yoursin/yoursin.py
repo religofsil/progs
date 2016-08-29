e66f5756575f69e5ec8157ef3592a37f2ef1dc04
Alexandra Ershova <religionofsilence@gmail.com>
2016-08-29 21:32:55 +0300

    Update yoursin.py

--- yoursin/yoursin.py
+++ yoursin/yoursin.py
@@ -2,13 +2,21 @@
 
 import random, codecs
 
-text=codecs.open('yoursin.txt', 'r', 'utf-8')
-sins=[]
+text = codecs.open('yoursin.txt', 'r', 'utf-8')
+sins = []
+
 for line in text:
     sins.append(line)
+    
 print u'Покайся, дитя моё. Назови любой номер, и Господь укажет тебе твой грех.'
-sin_number=input(u'Номер: ')
-if sin_number not in range(1, 1025):
-    sin_nimber=random.randint(1, 1024)
+
+try:
+    sin_number = input(u'Номер: ')
+    if sin_number not in range(1, 1025):
+        sin_number = random.randint(1, 1024)
+        
+except SyntaxError:
+    sin_number = random.randint(1, 1024)
+    
 print ''
-print u'Твой грех в том, что ты:\n'+sins[sin_number]
+print u'Твой грех в том, что ты:\n' + sins[sin_number - 1]
