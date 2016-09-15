b5f2871b787594388448b6aa1f14bdad0a07ebed
religofsil <religionofsilence@gmail.com>
2016-09-15 18:52:13 +0300

    update lastcourse

---
+++ lastcourse/2-3.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    print('Hello world, now with python 3!')
+f=open('C:/Python27/progs/machinelearning/part-of-speech.txt', 'r', encoding='utf-8')
+print(f)
+x=int(input('hi!'))
