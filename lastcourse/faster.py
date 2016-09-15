1f2eb5be11a35d952bb6ad0902411975003126e3
religofsil <religionofsilence@gmail.com>
2016-09-15 20:59:31 +0300

    update lastcourse

--- lastcourse/faster.py
+++ lastcourse/faster.py
@@ -1,14 +1,15 @@
 f = open('long_poem.txt', 'r', encoding='utf-8')
-text = f.read()
+text = f.read()  # читаем текст
 f.close()
-text = text.split('\n')
+text = text.split('\n')  # разбиваем текст на строки
 search = input('к чему надо подобрать рифму?\n')
 
 for i in text:
-    lastword = i.split(' ')[-1]
-    if lastword == search:
-        for n in range(-3, 3):
+    lastword = i.split(' ')[-1]  # последнее слово в строке
+    if lastword == search:  # ищем строки, в которых последнее слово -- то, которое мы ищем
+        for n in range(-3, 3):  # смотрим от нужной строки на 3 вверх и на 3 вниз
             num = text.index(i) + n
-            if text[num][-2:] == search[-2:]:
-                print(text[num])
+            if text[num][-2:] == search[
+                                 -2:]:  # если последние 2 символа строки совпадают с последними 2 символами запроса,
+                print(text[num])  # то выводим строку
         print('')
