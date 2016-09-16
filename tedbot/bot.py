b3def2997b05139b80f409a42cecba3f0ff3de33
religofsil <religionofsilence@gmail.com>
2016-09-16 17:12:45 +0300

    Create tedbot project.

--- tedbot/bot.py
+++ tedbot/bot.py
@@ -8,8 +8,13 @@ bot = telebot.TeleBot(config.token)
 def nameyourself(message):
     bot.send_message(message.chat.id, "Hi! I can send you an inspirational video from ted.com. Just type any topic. If you can't choose a topic, please type /random.")
 
+@bot.message_handler(commands=['random'])
+def nameyourself(message):
+    bot.send_message(message.chat.id, "Hi! I can send you an inspirational video from ted.com. Just type any topic. If you can't choose a topic, please type /random.")
+
+
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
